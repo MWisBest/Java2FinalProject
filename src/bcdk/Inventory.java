20f36077aaf8603caf607c96cf0e9800d2bd3732
Damian Olivo <olivodamian2@gmail.com>
2024-02-11 18:00:29 -0600

    Up-To-Date Inventory
    inventory works as intended with the current items available to player

--- src/bcdk/Inventory.java
+++ src/bcdk/Inventory.java
@@ -4,86 +4,84 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import bcdk.Items.Key;
+import bcdk.Items.Rocks;
+
 public class Inventory {
 	
-	// the List that will store all the player items 
-	private List<String> items;
+	// the Lists that will store all the player items 
+	private List<Key> keys;
+	private List<Rocks> rocks;
 	
 	/**
 	 * class constructor
 	 */
 	public Inventory() {
-		this.items = new ArrayList<>();
+		this.keys = new ArrayList<>();
+        this.rocks = new ArrayList<>();
 	}
 	
+
 	/**
-	 *  method to add a new item to the player inventory
-	 * @param item -  the name of the item to be added
+	 * 
+	 * @param key
 	 */
-	public void AddItem(String item) {
-		items.add(item);
-	}
+	 public void addKey(Key key) {
+		 keys.add(key);
+	 }
+
+	 /**
+	  * 
+	  * @param key
+	  */
+	 public void removeKey(Key key) {
+		 keys.remove(key);
+	 }
 	
+	 /**
+	  * 
+	  * @param rocks
+	  */
+	 public void addRocks(Rocks rocks) {
+		 this.rocks.add(rocks);
+	 }
+
+	 /**
+	  * 
+	  * @param rocks
+	  */
+	 public void removeRocks(Rocks rocks) {
+		 this.rocks.remove(rocks);
+	 }
+	 
 	/**
-	 * method to delete a item from the player inventory 
-	 * @param item - the name of the item to be removed
+	 * 
 	 */
-	public void RemoveItem(String item) {
-		boolean deleted = false; 
-		for(String i : items) {
-			if(i == item) {
-				items.remove(item);
-				deleted = true;
-				break;
-			} else {
-				deleted = false;
-			}
-		}
-		
-		if(deleted) {
-			System.out.println("Deleted Successfully");
-		} else {
-			System.out.println("Deletion Failed");
-		}
-		
-	}
-	
-	/**
-	 * Class that gets all the items that the player currently has 
-	 * @return the list of items stored 
-	 */
-	public List<String> CheckInventory() {
-		return items;
-	}
-	
-	/**
-	 * Saves the items in the List to the text file so user can retrieve his items in another game session
-	 * @param fileName -  the name of the txt file to retrieve the data from 
-	 */
-	public void SaveInventory(String fileName) {
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-			for(String item : items) {
-				writer.write(item);
-				writer.newLine();
-			}
-		} catch(IOException e) {
-			
-		}
-	}
-	
-	/**
-	 *  Retrieves the player items from a text file to put into the items list. 
-	 * @param fileName - the name of the txt file with inventory data
-	 */
-	public void LoadInventory(String fileName) {
-		try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-			String line;
-			while((line = reader.readLine()) != null) {
-				items.add(line);
-			}
-		} catch(IOException e) {
-			
-		}
-	} 
+	 public void displayKeys() {
+	        System.out.println("Keys in Inventory:");
+	        if(!keys.isEmpty()) {
+	        	for (Key key : keys) {
+	            System.out.println("- " + key.KeyDescription);
+	        	}
+	        } else {
+	        	System.out.println("You do not own any keys");
+	        }
+	        
+	 }
+	 
+	 /**
+	  * 
+	  */
+	 public void displayRocks() {
+	        System.out.println("Rocks in Inventory:");
+	        if(!rocks.isEmpty()) {
+	        	for (Rocks rock : rocks) {
+	            System.out.println("- " + rock.RockCount + " rocks left");
+	        	}
+	        } else {
+	        	System.out.println("You do not have any rocks");
+	        }
+	        
+	    }
 	
 }
