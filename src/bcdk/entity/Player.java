396d55b6141c455ddcd59c2bc2699df8e41e6e57
Kyle Repinski <repinski23@gmail.com>
2024-03-08 08:19:28 -0600

    JavaDoc: Player

--- src/bcdk/entity/Player.java
+++ src/bcdk/entity/Player.java
@@ -31,10 +31,21 @@ public class Player extends Entities {
 		this.playerInventory = new Inventory();
 	}
 	
+	/**
+	 * Adds a Checkpoint to the list of Checkpoints the Player has reached.
+	 * 
+	 * @param checkpoint Checkpoint to add.
+	 */
 	public void addCheckpointReached(Checkpoint checkpoint) {
 		this.checkpointsReached.add(checkpoint);
 	}
 	
+	/**
+	 * Check if the Player has reached a Checkpoint
+	 * 
+	 * @param checkpoint the Checkpoint to look for
+	 * @return true if the Player has this Checkpoint, or false if not
+	 */
 	public boolean checkForCheckpoint(Checkpoint checkpoint) {
 		for (Checkpoint cp : this.checkpointsReached) {
 			if (cp.equals(checkpoint)) {
@@ -44,14 +55,26 @@ public class Player extends Entities {
 		return false;
 	}
 
+	/**
+	 * @return the Room the player is in
+	 */
 	public Room getLocation() {
 		return location;
 	}
 
+	/**
+	 * Set the Room the player is in.
+	 * 
+	 * @param room new Room the player is located in.
+	 */
 	public void setLocation(Room room) {
 		this.location = room;
 	}
 
+	/**
+	 * Get the Player's Inventory
+	 * @return Player's Inventory
+	 */
 	public Inventory getInventory() {
 		return playerInventory;
 	}
